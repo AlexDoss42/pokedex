f6d55abb304e97538285336239bf01caf1b4f8b0
AlexDoss42 <alex.doss42@gmail.com>
2024-06-05 16:51:12 -0600

    builds out a basic pokemon card

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const PokemonCard = () => {
+  return (
+    <div>
+        <h1>Pokemon name</h1>
+        <img>The pokemon sprite</img>
+        <p>Pokedex Dscription</p>
+    </div>
+  )
+}
