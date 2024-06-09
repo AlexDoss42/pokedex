387c8328a69eab88f6d72818718d8880f3117f31
AlexDoss42 <alex.doss42@gmail.com>
2024-06-08 23:31:22 -0600

    adds alt tag to img so it is Accessibility compliant

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -4,7 +4,7 @@ export const PokemonCard = () => {
   return (
     <div>
         <h1>Pokemon name</h1>
-        <img>The pokemon sprite</img>
+        <img alt="charizard sprite">The pokemon sprite</img>
         <p>Pokedex Dscription</p>
     </div>
   )
