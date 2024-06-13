669d96e83501dc03b4cd4a027e034ecfe5480e46
AlexDoss42 <alex.doss42@gmail.com>
2024-06-12 22:48:56 -0600

    adds the pokemon red background and cleans up the app styling a bit

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { PokemonCard } from './components/PokemonCard';
 
 function App() {
   return (
-    <div className="App">
-        <p>
+    <div className="App" style={{ backgroundColor: "#ff0000" }}>
+        <p style={{ textAlign: "center", marginTop: "0px", paddingTop: "10px"}}>
           Gotta Catch Em All
         </p>
         <PokemonCard />
