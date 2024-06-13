8b49c2225efd7e73c98e9cafe3e72a540166ef55
AlexDoss42 <alex.doss42@gmail.com>
2024-06-12 23:01:50 -0600

    adds basic jsx for types

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import data from '../charizardData.json'
-
+console.log(data[0].types[0].type.name)
 export const PokemonCard = () => {
   return (
     <div style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
         <h1>Name</h1>
         <p>{data[0].name}</p>
+        <div className='types'>
+        <a href={data[0].types[0].type.url}>{data[0].types[0].type.name}</a>
+        <a href={data[0].types[1].type.url}>{data[0].types[1].type.name}</a>
+        </div>
         <div className='defaultSprites'>
-        <img src={data[0].sprites.back_default} alt="shiny Charizard" />
-        <img src={data[0].sprites.front_default} alt="shiny Charizard" />
+        <img src={data[0].sprites.back_default} alt="Normal Back of Charizard" />
+        <img src={data[0].sprites.front_default} alt="Normal Front of Charizard" />
         </div>
         <div className='shinySprites'>
-        <img src={data[0].sprites.back_shiny} alt="shiny Charizard" />
-        <img src={data[0].sprites.front_shiny} alt="shiny Charizard" />
+        <img src={data[0].sprites.back_shiny} alt="shiny back Charizard" />
+        <img src={data[0].sprites.front_shiny} alt="shiny front Charizard" />
         </div>
     </div>
   )
