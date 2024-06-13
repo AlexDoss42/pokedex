f369e16e30a518c102339388c421004d8ccc04b8
AlexDoss42 <alex.doss42@gmail.com>
2024-06-12 22:55:40 -0600

    adds back and both default sprites

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -6,7 +6,14 @@ export const PokemonCard = () => {
     <div style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
         <h1>Name</h1>
         <p>{data[0].name}</p>
+        <div className='defaultSprites'>
+        <img src={data[0].sprites.back_default} alt="shiny Charizard" />
+        <img src={data[0].sprites.front_default} alt="shiny Charizard" />
+        </div>
+        <div className='shinySprites'>
+        <img src={data[0].sprites.back_shiny} alt="shiny Charizard" />
         <img src={data[0].sprites.front_shiny} alt="shiny Charizard" />
+        </div>
     </div>
   )
 }
