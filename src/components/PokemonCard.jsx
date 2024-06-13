cbf1381460395e74aa9894a90a018725c36e1b71
AlexDoss42 <alex.doss42@gmail.com>
2024-06-12 22:46:25 -0600

    adds some styling to the pokemon card

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -3,7 +3,7 @@ import data from '../charizardData.json'
 
 export const PokemonCard = () => {
   return (
-    <div>
+    <div style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
         <h1>Name</h1>
         <p>{data[0].name}</p>
         <img src={data[0].sprites.front_shiny} alt="shiny Charizard" />
