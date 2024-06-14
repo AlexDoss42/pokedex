683ec539fb86b92f8603df8285e0d644d300e72a
AlexDoss42 <alex.doss42@gmail.com>
2024-06-13 22:25:27 -0600

    sets up the server for name search

--- server/index.js
+++ server/index.js
@@ -4,10 +4,19 @@ const app = express();
 
 app.use(express.json());
 
-app.get('https://pokeapi.co/api/v2/pokemon/:pokemon_name', async (req, res) => {
+app.get('pokemon/:pokemon_name', async (req, res) => {
     try {
         const {pokemon_name} = req.params;
-    } catch {
-        
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon_name}`)
+        const pokemon = response.data;
+
+
+        res.render('pokemon', pokemon);
+
+    } catch(e) {
+        res.send(404).render('error', {message: "Pokemon not found!"})
     }
-})
+});
+
+const PORT = 3030;;
+app.listen(PORT, () => {console.log(`I CHOOSE YOU ${PORT}`)});
