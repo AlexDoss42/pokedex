db1217bd87f1e18ee3340ab348267a68686b106e
AlexDoss42 <alex.doss42@gmail.com>
2024-06-17 21:00:33 -0600

    sets up state and conditional to render default and shiny pokemon

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import data from '../charizardData.json'
 console.log(data[0])
 export const PokemonCard = () => {
+  const [showShiny, setShowShiny] = useState(true);
+
+  const handleShiny = () => {
+    setShowShiny(!!showShiny)
+  }
   return (
     <div style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
         <h1>Name</h1>
         <p>{data[0].name}</p>
         <div className='types'>
-        <a href={data[0].types[0].type.url}>{data[0].types[0].type.name}</a>
-        <a href={data[0].types[1].type.url}>{data[0].types[1].type.name}</a>
+          <a href={data[0].types[0].type.url}>{data[0].types[0].type.name}</a>
+          <a href={data[0].types[1].type.url}>{data[0].types[1].type.name}</a>
         </div>
-        <div className='defaultSprites'>
-        <img src={data[0].sprites.back_default} alt="Normal Back of Charizard" />
-        <img src={data[0].sprites.front_default} alt="Normal Front of Charizard" />
-        </div>
+        
+        {showShiny ? 
         <div className='shinySprites'>
-        <img src={data[0].sprites.back_shiny} alt="shiny back Charizard" />
-        <img src={data[0].sprites.front_shiny} alt="shiny front Charizard" />
-        </div>
+          <img src={data[0].sprites.back_shiny} alt="shiny back Charizard" />
+          <img src={data[0].sprites.front_shiny} alt="shiny front Charizard" />
+        </div> : 
+        <div className='defaultSprites'>
+          <img src={data[0].sprites.back_default} alt="Normal Back of Charizard" />
+          <img src={data[0].sprites.front_default} alt="Normal Front of Charizard" />
+        </div>}
     </div>
   )
 }
