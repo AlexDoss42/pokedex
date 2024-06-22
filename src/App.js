a37f4b65ff902b55a4a6dc7c8c361602bc3caff9
AlexDoss42 <alex.doss42@gmail.com>
2024-06-21 22:44:54 -0600

    threw in an easter egg for the placeholder in the search bar

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <p style={{ textAlign: "center", marginTop: "0px", paddingTop: "10px"}}>
           Gotta Catch Em All
         </p>
-        <input style={{ width: "360px", margin: '10px auto' }} type='text' id="name" value={pokemonName} onChange={e => setPokemonName(e.target.value)} />
+        <input placeholder="WHOSE THAT POKEMON!!!" style={{ width: "360px", margin: '10px auto' }} type='text' id="name" value={pokemonName} onChange={e => setPokemonName(e.target.value)} />
         <button onClick={() => handleSearch()} style={{ backgroundColor: "#1A58CA", border: "none", borderRadius: '5px', color: "#fff", height: "30px", width: "90px", fontSize: "20px", margin: "10px auto"}}>Search</button>
         <PokemonCard pokeData={pokeData} />
     </div>
