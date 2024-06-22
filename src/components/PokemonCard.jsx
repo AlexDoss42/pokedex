a7fe25f0a822de4a99b532c3127771a2e0f8117d
AlexDoss42 <alex.doss42@gmail.com>
2024-06-21 23:07:37 -0600

    conditionally render the pokecard once you hit search but broken

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,31 +1,36 @@
 import React, { useState } from 'react'
-import data from '../charizardData.json'
-console.log(data[0])
-export const PokemonCard = () => {
-  const [showShiny, setShowShiny] = useState(true);
+// import data from '../charizardData.json'
 
+
+export const PokemonCard = (pokeData) => {
+  const [showShiny, setShowShiny] = useState(true);
+  console.log(pokeData[0])
   const handleShiny = () => {
     setShowShiny(!showShiny)
   }
   return (
+    // {pokeData ?
     <div style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
         <p>Name</p>
-        <h2>{data[0].name}</h2>
+        <h2>{pokeData[0].name}</h2>
         <div className='types'>
-          <a href={data[0].types[0].type.url}>{data[0].types[0].type.name}</a>
-          <a href={data[0].types[1].type.url}>{data[0].types[1].type.name}</a>
+          <a href={pokeData[0].types[0].type.url}>{pokeData[0].types[0].type.name}</a>
+          <a href={pokeData[0].types[1].type.url}>{pokeData[0].types[1].type.name}</a>
         </div>
         
         {showShiny ? 
         <div className='shinySprites'>
-          <img src={data[0].sprites.back_shiny} alt="shiny back Charizard" />
-          <img src={data[0].sprites.front_shiny} alt="shiny front Charizard" />
+          <img src={pokeData[0].sprites.back_shiny} alt="shiny back Charizard" />
+          <img src={pokeData[0].sprites.front_shiny} alt="shiny front Charizard" />
         </div> : 
         <div className='defaultSprites'>
-          <img src={data[0].sprites.back_default} alt="Normal Back of Charizard" />
-          <img src={data[0].sprites.front_default} alt="Normal Front of Charizard" />
+          <img src={pokeData[0].sprites.back_default} alt="Normal Back of Charizard" />
+          <img src={pokeData[0].sprites.front_default} alt="Normal Front of Charizard" />
         </div>}
         <button onClick={() => handleShiny()} style={{ backgroundColor: "#1A58CA", border: "none", borderRadius: '5px', color: "#fff", height: "30px", width: "150px", fontSize: "20px"}}>Toggle Shiny</button>
     </div>
+    //  : 
+    // <></>
+  // }
   )
 }
