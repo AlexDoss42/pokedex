87b0076d364500e05b16188cbfbb05c2c4af3fa7
AlexDoss42 <alex.doss42@gmail.com>
2024-06-21 23:00:25 -0600

    successfully gets data from API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import './App.css';
 import React, { useState }  from 'react';
 import { PokemonCard } from './components/PokemonCard';
-import { response } from 'express';
-import axios from 'axios';
+// import { response } from 'express';
+// import axios from 'axios';
 
 
 function App() {
   const [pokemonName, setPokemonName] = useState("");
   const [pokeData, setPokeData] = useState("")
 
-  const handleSearch = () => {
-      axios.get(`pokemon/${pokemonName}`).then(response => setPokeData(response.data))
-    console.log(response.data);
+  // const handleSearch = () => {
+  //     axios.get(`pokemon/${pokemonName}`).then(response => setPokeData(response.data))
+  //   console.log(response.data);
+  // }
+
+  const handleSearch = (pokemonName) => {
+    let pokemonNameLower = pokemonName.toLowerCase()
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNameLower}`)
+    .then((response) => response.json()
+    .then((json) => {
+      console.log(json);
+      setPokeData(json);
+    }))
   }
+
+
   return (
     <div className="App" style={{ backgroundColor: "#ff0000", display: "flex", flexDirection: "column", alignContent: "center" }}>
         <p style={{ textAlign: "center", marginTop: "0px", paddingTop: "10px"}}>
           Gotta Catch Em All
         </p>
         <input placeholder="WHOSE THAT POKEMON!!!" style={{ width: "360px", margin: '10px auto' }} type='text' id="name" value={pokemonName} onChange={e => setPokemonName(e.target.value)} />
-        <button onClick={() => handleSearch()} style={{ backgroundColor: "#1A58CA", border: "none", borderRadius: '5px', color: "#fff", height: "30px", width: "90px", fontSize: "20px", margin: "10px auto"}}>Search</button>
+        <button onClick={() => handleSearch(pokemonName)} style={{ backgroundColor: "#1A58CA", border: "none", borderRadius: '5px', color: "#fff", height: "30px", width: "90px", fontSize: "20px", margin: "10px auto"}}>Search</button>
         <PokemonCard pokeData={pokeData} />
     </div>
   );
