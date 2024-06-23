a33899c66da0ee3bdcffed0a736672fba2d98972
AlexDoss42 <alex.doss42@gmail.com>
2024-06-22 18:09:35 -0600

    destructures data a bit for cleaner code

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -3,34 +3,33 @@ import React, { useState } from 'react'
 
 
 export const PokemonCard = (rawData) => {
+  const rawData2 = rawData.rawData;
+  console.log(rawData2)
+  const { types, sprites, name } = rawData2;
   const [showShiny, setShowShiny] = useState(true);
-  console.log(rawData.rawData)
+
   const handleShiny = () => {
     setShowShiny(!showShiny)
   }
   return (
-    // {rawData.rawData ?
     <div style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
         <p>Name</p>
-        <h2>{rawData.rawData.name}</h2>
+        <h2>{name}</h2>
         <div className='types'>
-          <a href={rawData.rawData.types[0].type.url}>{rawData.rawData.types[0].type.name}</a>
-          <a href={rawData.rawData.types[1].type.url}>{rawData.rawData.types[1].type.name}</a>
+          <a href={types[0].type.url}>{types[0].type.name}</a>
+          <a href={types[1].type.url}>{types[1].type.name}</a>
         </div>
         
         {showShiny ? 
         <div className='shinySprites'>
-          <img src={rawData.rawData.sprites.back_shiny} alt="shiny back Charizard" />
-          <img src={rawData.rawData.sprites.front_shiny} alt="shiny front Charizard" />
+          <img src={sprites.back_shiny} alt="shiny back Charizard" />
+          <img src={sprites.front_shiny} alt="shiny front Charizard" />
         </div> : 
         <div className='defaultSprites'>
-          <img src={rawData.rawData.sprites.back_default} alt="Normal Back of Charizard" />
-          <img src={rawData.rawData.sprites.front_default} alt="Normal Front of Charizard" />
+          <img src={sprites.back_default} alt="Normal Back of Charizard" />
+          <img src={sprites.front_default} alt="Normal Front of Charizard" />
         </div>}
         <button onClick={() => handleShiny()} style={{ backgroundColor: "#1A58CA", border: "none", borderRadius: '5px', color: "#fff", height: "30px", width: "150px", fontSize: "20px"}}>Toggle Shiny</button>
     </div>
-    //  : 
-    // <></>
-  // }
   )
 }
