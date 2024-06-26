c9fd60cffbcceb7488c66ac2a29d684483807895
AlexDoss42 <alex.doss42@gmail.com>
2024-06-26 00:21:11 -0600

    conditionally styles the type background based off of the type name that is passed to the tile

--- src/components/TypeTile.jsx
+++ src/components/TypeTile.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+
 
 export const TypeTile = ({typeData}) => {
     let { type } = typeData;
+    const getBackgroundColor = () =>{
+        switch (type.name) {
+          case 'fire':
+            return {backgroundColor: 'red'}
+          case 'water':
+            return {backgroundColor: 'blue'}
+            
+          case 'grass':
+           return { backgroundColor: 'green'}
+           
+          case 'electric':
+            return {backgroundColor: 'yellow'}
+            
+        default: 
+            return {backgroundColor: 'grey'}
+        
+      }
+}
+
+const [typeStyles, setTypeStyles] = useState({})
+
+useEffect(() => {
+    setTypeStyles(() => getBackgroundColor())
+}, [type.name])
+
+    
+
+console.log(getBackgroundColor());
+
   return (
-    <a className="type" href={type.url}>{type.name}</a>
+    <a className="type" style={typeStyles} href={type.url}>{type.name}</a>
   )
 }
