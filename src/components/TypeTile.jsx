8d69498e90b78c0175f119a753b911798f75648a
AlexDoss42 <alex.doss42@gmail.com>
2024-06-27 16:54:12 -0600

    gets all the types with the game approriate hex code lined up for bg color

--- src/components/TypeTile.jsx
+++ src/components/TypeTile.jsx
@@ -8,17 +8,42 @@ export const TypeTile = ({typeData}) => {
     
     const getBackgroundColor = () =>{
         switch (type.name) {
+          case 'normal':
+            return {backgroundColor: '#AAAA99'}
           case 'fire':
-            return {backgroundColor: 'red'}
+            return {backgroundColor: '#EE4523'}
           case 'water':
-            return {backgroundColor: 'blue'}
-            
+            return {backgroundColor: '#4F99FF'}
           case 'grass':
-           return { backgroundColor: 'green'}
-           
+           return { backgroundColor: '#77CC54'}
           case 'electric':
-            return {backgroundColor: 'yellow'}
-            
+            return {backgroundColor: '#F5CC34'}
+          case 'fighting':
+            return {backgroundColor: '#B95443'}
+          case 'ice':
+            return {backgroundColor: '#66CBFF'}
+          case 'poison':
+           return { backgroundColor: '#AA5599'}
+          case 'ground':
+            return {backgroundColor: '#DDBB54'}
+          case 'flying':
+            return {backgroundColor: '#8899FF'}
+          case 'pyschic':
+            return {backgroundColor: '#F05599'}
+          case 'bug':
+            return {backgroundColor: '#AABB23'}
+          case 'rock':
+           return { backgroundColor: '#BBAA66'}
+          case 'ghost':
+            return {backgroundColor: '#6666BB'}
+          case 'dragon':
+            return {backgroundColor: '#7766EE'}
+          case 'dark':
+            return {backgroundColor: '#775544'}
+          case 'steel':
+           return { backgroundColor: '#AAAABB'}
+          case 'fairy':
+            return {backgroundColor: '#EE98EE'}
         default: 
             return {backgroundColor: 'grey'}
         
