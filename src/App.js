70929f005a190b93f8fc5e72684322dafce5f259
AlexDoss42 <alex.doss42@gmail.com>
2024-06-28 16:59:16 -0600

    adds error handling for a failed search

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { PokemonCard } from './components/PokemonCard';
 function App() {
   const [pokemonName, setPokemonName] = useState("");
   const [pokeData, setPokeData] = useState("")
+  const [itsPikachu, setItsPikachu] = useState(false);
 
   const handleSearch = (pokemonName) => {
     let pokemonNameLower = pokemonName.toLowerCase()
@@ -15,7 +16,7 @@ function App() {
     .then((response) => response.json()
     .then((json) => {
       setPokeData(json);
-    }))
+    })).catch((error) => window.alert("Oops it looks like that isn't a pokemon"))
   }
 
 
@@ -27,6 +28,21 @@ function App() {
         <input placeholder="WHOSE THAT POKEMON!!!" style={{ width: "360px", margin: '10px auto' }} type='text' id="name" value={pokemonName} onChange={e => setPokemonName(e.target.value)} />
         <button onClick={() => handleSearch(pokemonName)} style={{ backgroundColor: "#1A58CA", border: "none", borderRadius: '5px', color: "#fff", height: "30px", width: "90px", fontSize: "20px", margin: "10px auto"}}>Search</button>
         {pokeData ? <PokemonCard rawData={pokeData} /> : <></> }
+        {itsPikachu ? 
+        <iframe 
+          width="560" 
+          height="315" 
+          src="https://www.youtube.com/embed/WSGV_n6H1n0?si=FMFf0xmAbDc_JyeY" 
+          title="YouTube video player" 
+          frameborder="0" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+          referrerpolicy="strict-origin-when-cross-origin" 
+          allowfullscreen
+        >
+        </iframe>
+        : 
+        <></>
+        }
     </div>
   );
 }
